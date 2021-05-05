a806146d8f815138b2d9c533ed27cf731d5c8aee
anushamamidala <anushamamidala817@gmail.com>
2021-05-04 23:26:44 -0400

    added

---
+++ src/app/template-form/template-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.css']
+})
+export class TemplateFormComponent implements OnInit {
+  model: any = {};
+  submitted: boolean = false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.model)
+  }
+}
